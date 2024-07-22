0de7ef0ee2ebf48af61a57ac35ce5eb5ca1ed4a4
Lokeshwaran29 <84411298+lokeshwaran105@users.noreply.github.com>
2024-07-22 16:48:38 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function correctAnswer(){
+    document.getElementsByClassName('option-name')[2].style.backgroundColor = "rgb(25, 230, 25)";
+    document.getElementsByClassName('crt')[0].style.backgroundColor = "rgb(25, 230, 25)";
+}
+
+
+document.getElementsByClassName('wrg1')[0].style.backgroundColor = "rgb(25, 230, 25)";
